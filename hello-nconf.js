e3c4396726550f3efd3a21fad06047ae0e75da14
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-12-01 15:46:48 +0000

    hello-nconf.js

--- hello-nconf.js
+++ hello-nconf.js
@@ -1,6 +1,6 @@
   var fs    = require('fs'),
       nconf = require('nconf');
- 
+
   // The order in which you attach the configuration sources determines their priority in the hierarchy.
   // Setup nconf to use (in-order):
   //   1. Command-line arguments
@@ -10,36 +10,37 @@
   nconf.argv() // Loads process.argv using yargs. If options is supplied it is passed along to yargs.
    .env('__')      // Loads process.env into the hierarchy
    .file({ file: './config.json' });
- 
+
   // See "node hello-nconf.js --conf='./config-2.json'"
   nconf.file(nconf.get('conf'));
 
-/*
-  nconf.env() 
-   .file({ file: './config.json' })
-   .argv();  
-*/
-/*
-  nconf
-   .file({ file: './config.json' })
-   .argv()
-   .env() ;
-*/
+  /*
+    nconf.env()
+     .file({ file: './config.json' })
+     .argv();
+  */
+  /*
+    nconf
+     .file({ file: './config.json' })
+     .argv()
+     .env() ;
+  */
   //
   // Set a few variables on `nconf`.
   //
   nconf.set('database:host', '127.0.0.1');
   nconf.set('database:port', 4000);
- 
+
+  console.log('foo: ' + nconf.get('foo'));
+  console.log('NODE_ENV: ' + nconf.get('NODE_ENV'));
   //
   // Get the entire database object from nconf. This will output
   // { host: '127.0.0.1', port: 5984 }
   //
-  console.log('foo: ' + nconf.get('foo'));
-  console.log('NODE_ENV: ' + nconf.get('NODE_ENV'));
   console.log('database: ' + JSON.stringify(nconf.get('database')));
 
-  // It is important to note that setting keys in the File engine will not be persisted 
+  // It is important to note that setting keys in the File engine
+  // will not be persisted
   // to disk until a call to .save() is made
   // Save the configuration object to disk
   //
